1bc7db39ffea459492f5dbfe1299b5865bebf7d9
kevinkii <kevinmtaing@Monireangseys-MacBook-Pro.local>
2019-09-30 15:52:55 -0700

    finished up

--- dataquest5-summary.py
+++ dataquest5-summary.py
@@ -157,3 +157,143 @@ for sp in range(0,6):
 plt.show()
 
 #%% [markdown]
+# ## Conditional Plots
+#
+# Seaborn: 
+# * Is built on top of matplotlib.
+# * Has good support for more complex plots.
+# * Attractive default styles.
+# * Integrates well with the pandas library.
+#
+#
+# `seaborn.set_style()` function to change the default seaborn style sheet:
+#
+# **darkgrid**: Coordinate grid displayed, dark background color
+#
+# **whitegrid**: Coordinate grid displayed, white background color
+#
+# **dark**: Coordinate grid hidden, dark background color
+#
+# **white**: Coordinate grid hidden, white background color
+# 
+# **ticks**: Coordinate grid hidden, white background color, ticks visible
+
+#%%
+import pandas as pd
+cols = ["Survived","Pclass","Sex","Age","SibSp","Parch","Fare","Embarked"]
+titanic = pd.read_csv("train.csv")
+titanic = titanic[cols].dropna()
+
+#%%
+# Drawing the distribution plots (historgram)
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.distplot(titanic["Age"])
+plt.show()
+
+#%%
+# Drawing Kernel Density Plot (a smoother version of the histogram)
+
+sns.kdeplot(titanic["Age"], shade = True)
+plt.xlabel("Age")
+
+#%%
+# Setting the style of kernel density plot
+
+sns.set_style('white')
+sns.kdeplot(titanic['Age'], shade=True)
+sns.despine(left=True, bottom=True)
+plt.xlabel('Age')
+
+#%%
+
+# Condition on unique values of the "Pclass" column
+g = sns.FacetGrid(titanic, col="Pclass", size = 6)
+
+# For each subset of values, generate a kernel density plot of the "Age" columns
+g.map(sns.kdeplot, "Age", shade=True)
+sns.despine(left=True,bottom=True)
+
+# *size* speicifies the height in inches of each plot
+
+#%%
+# adding hue as shade in the conditional plot
+
+g = sns.FacetGrid(titanic, col="Survived", row="Pclass", hue="Sex", size=3)
+g.map(sns.kdeplot, "Age", shade=True)
+g.add_legend()
+sns.despine(left=True, bottom=True)
+plt.show()
+
+#%% [markdown]
+# # Visualizing Geographic Data
+
+#%%
+import pandas as pd
+airlines = pd.read_csv("airlines.csv")
+airports = pd.read_csv("airports.csv")
+routes = pd.read_csv("routes.csv")
+
+#%%
+# Drawing a 2D map
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+m = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80, llcrnrlon=-180, urcrnrlon=180)
+longitudes = airports["longitude"].tolist()
+latitudes = airports["latitude"].tolist()
+x, y = m(longitudes, latitudes)
+
+# drawing using longtitudes & latitudes
+m.scatter(x,y,s=1) # 's' represents the marker
+
+# drawing the outlines of the coastline
+m.drawcoastlines()
+plt.show()
+
+#%%
+# Creating the basemap instance with matplotlib.pyplot and mpl_toolkits.basemap
+
+fig,ax = plt.subplots(figsize=(15,20))
+plt.title("Scaled Up Earth With Coastlines")
+
+m = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80, llcrnrlon=-180, urcrnrlon=180)
+longitudes = airports["longitude"].tolist()
+latitudes = airports["latitude"].tolist()
+x, y = m(longitudes, latitudes)
+m.scatter(x, y, s=1)
+m.drawcoastlines()
+plt.show()
+
+#%%
+# Drawing a 3D map
+
+import pandas as pd
+geo_routes = pd.read_csv("geo_routes.csv")
+geo_routes.info()
+
+next(geo_routes.iterrows())
+
+fig, ax = plt.subplots(figsize=(15,20))
+m = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80, llcrnrlon=-180, urcrnrlon=180)
+m.drawcoastlines()
+
+def create_great_circles(df):
+    for index, row in df.iterrows():
+        end_lat, start_lat = row['end_lat'], row['start_lat']
+        end_lon, start_lon = row['end_lon'], row['start_lon']
+        if abs(end_lat - start_lat) < 180:
+            if abs(end_lon - start_lon) < 180:
+                m.drawgreatcircle(start_lon, start_lat, end_lon, end_lat)
+
+dfw = geo_routes[geo_routes['source'] == "DFW"]
+create_great_circles(dfw)
+plt.show()
+
+#%% [markdown]
+# `basemap.drawgreatcircle()` method to display a great circle between 2 points. The basemap.drawgreatcircle() method requires four parameters in the following order:
+# * lon1 - longitude of the starting point.
+# * lat1 - latitude of the starting point.
+# * lon2 - longitude of the ending point.
+# * lat2 - latitude of the ending point.
